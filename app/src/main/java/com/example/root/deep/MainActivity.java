09cfdc405618ca2d1918e5ac8759e5a55eaf5a7c
彭飞飞 <feifei.peng@dianrong.com>
2017-07-03 13:38:36 +0800

    JSRoad

--- app/src/main/java/com/example/root/deep/MainActivity.java
+++ app/src/main/java/com/example/root/deep/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.root.deep.base.BaseActivity;
+import com.example.root.deep.deeplink.WebViewActivity;
+import com.example.root.deep.jsbridge.BridgeWebActivity;
+import com.example.root.deep.jsroad.RoadWebActivity;
 
 import butterknife.OnClick;
 
@@ -28,5 +31,10 @@ public class MainActivity extends BaseActivity {
     @OnClick(R.id.textBridge)
     public void onViewClicked() {
         startActivity(new Intent(this, BridgeWebActivity.class));
+    }
+
+    @OnClick(R.id.textRoad)
+    public void onRoadClicked() {
+        startActivity(new Intent(this, RoadWebActivity.class));
     }
 }
